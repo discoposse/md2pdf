119fcf215f5f12cd75443a3f9564e2ca228e9e84
discoposse <eric@discoposse.com>
2024-08-19 14:35:38 -0400

    updating landing page content

--- src/App/Container/Hooks/InitialText.js
+++ src/App/Container/Hooks/InitialText.js
@@ -1,10 +1,10 @@
+export const initialText = `# Markdown2PDF 
+
 ## How to use md2pdf?
 1. Click button choose \`.md\` file.
 2. Edit in editor (left panel).
-3. Click **Transform**!
+3. Click **Generate PDF**!
 4. Switch 'Destination' to **Save as PDF**.
 4. **Chrome recommended**
 
-## Tips
-- \`Resize\` the layout what you want.
-- After click \`Transform\` button, inverse the checkbox of **'Headers and Footers'**. 
+`;
